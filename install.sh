3313d2b513fea26f42ddca2202531b5e6788d089
Matthias Guenther <matthias@wikimatze.de>
2014-05-04 08:33:00 +0200

    Symlink vimrc

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/matthias-guenther/vimfiles.git ~/.vim
 mkdir ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-mv ~/.vim/.vimrc ~/
+ln -sf ~/.vim/.vimrc ~/.vimrc
 
