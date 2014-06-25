8c3e4190e13b815be79f98c76f6bbf9f03576a01
Matthias Guenther <matthias@wikimatze.de>
2014-06-25 09:44:01 +0200

    The autoload directory is already included in a checkout

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/matthias-guenther/vimfiles.git ~/.vim
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
 ln -sf ~/.vim/.vimrc ~/.vimrc
 
-echo "Start vim and run :PlugInstall"
 echo "$ cd ~/.vim/plugged/vimproc.vim && make"
+echo "Start vim and run :PlugInstall"
 
