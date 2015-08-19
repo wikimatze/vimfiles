1321b26c65b7cd765d2358ca78e8281167a6089f
Matthias Guenther <matthias@wikimatze.de>
2015-08-19 17:17:18 +0200

    Update install

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-git clone https://github.com/matthias-guenther/vimfiles.git ~/.vim
+git clone https://github.com/wikimatze/vimfiles.git ~/.vim
 ln -sf ~/.vim/.vimrc ~/.vimrc
 mkdir -p ~/.fonts
 
 echo "Grab mthes10 thesaurus ..."
-cd /tmp && wget http://www.gutenberg.org/dirs/etext02/mthes10.zip -O mthesaur.zip && unzip mthesaur.zip && mv mthesaur.txt ~/.vim/thesaurus/
+cd /tmp && wget http://www.gutenberg.org/files/3202/old/2mthes10.zip -O mthesaur.zip && unzip mthesaur.zip && mv mthesaur.txt ~/.vim/thesaurus/
 echo "... done!"
 
 echo "To have python and ruby installed, run `sudo apt-get build-dep vim` ..."
@@ -14,5 +13,5 @@ echo "... done!"
 
 echo "Start vim and run :PlugInstall"
 
-echo "Next run `cd ~/.fonts && curl -fLo DroidSansMonoForPowerlinePlusNerdFileTypes.otf https://raw.githubusercontent.com/ryanoasis/nerd-filetype-glyphs-fonts-patcher/master/patched-fonts/Droid%20Sans%20Mono%20for%20Powerline%20Plus%20Nerd%20File%20Types.otf && sudo chmod 777 DroidSansMonoForPowerlinePlusNerdFileTypes.otf` to install fonts"
+echo "Next run `cd ~/.fonts && curl -fLo DroidSansMonoForPowerlinePlusNerdFileTypes.otf https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline%20Plus%20Nerd%20File%20Types.otf && sudo chmod 777 DroidSansMonoForPowerlinePlusNerdFileTypes.otf` to install fonts"
 
